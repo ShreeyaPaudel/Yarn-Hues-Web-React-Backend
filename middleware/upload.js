019f96ee8fd9e57003b0acecdb717c6d1fc1d98c
Shreeya Paudel <shreeya.paudel.8e@gmail.com>
2025-03-03 14:30:47 +0545

    upload js

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,17 +1,16 @@
 const multer = require("multer");
 const path = require("path");
 
-// Storage engine
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/"); // Uploads folder
+    cb(null, "uploads/"); 
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
   },
 });
 
-// File filter to allow only images
 const fileFilter = (req, file, cb) => {
   const allowedFileTypes = /jpeg|jpg|png|gif/;
   const extname = allowedFileTypes.test(path.extname(file.originalname).toLowerCase());
@@ -24,10 +23,10 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-// Upload middleware
+
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // Limit file size to 5MB
+  limits: { fileSize: 5 * 1024 * 1024 }, 
   fileFilter: fileFilter,
 });
 
