9ea306129c0dafaf58267a9250320d85de50c4cc
Shreeya Paudel <shreeya.paudel.8e@gmail.com>
2025-03-03 14:34:39 +0545

    routes for user, product, cart and order updated

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const cartController = require("../controllers/cartController");
 const { verifyToken } = require("../middleware/authMiddleware");
 
-// Only authenticated users can manage their cart
+
 router.post("/", verifyToken, cartController.addToCart);
 router.get("/:userId", verifyToken, cartController.getCartItems);
 router.put("/:id", verifyToken, cartController.updateCartItem);
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -4,7 +4,7 @@ const productController = require("../controllers/productController");
 const upload = require("../middleware/upload");
 const { verifyToken, verifyAdmin } = require("../middleware/authMiddleware");
 
-// Protect admin routes (only admins can manage products)
+
 router.post("/", verifyAdmin, upload.single("image"), productController.createProduct);
 router.get("/", productController.getAllProducts);
 router.get("/:id", productController.getProductById);
