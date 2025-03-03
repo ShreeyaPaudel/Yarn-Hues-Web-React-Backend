0972b484d6fdc50dac0f9506b4333a8165f20438
Shreeya Paudel <shreeya.paudel.8e@gmail.com>
2025-03-03 14:28:57 +0545

    queries

--- database/config.js
+++ database/config.js
@@ -1,14 +1,13 @@
 const { Sequelize } = require("sequelize");
-require("dotenv").config(); // Load environment variables
+require("dotenv").config(); 
 
-// Initialize Sequelize with PostgreSQL connection
+
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: "postgres",
-  logging: false, // Set to true to see raw SQL queries
+  logging: false,
 });
 
-// Test Database Connection
 sequelize.authenticate()
   .then(() => console.log("PostgreSQL connected successfully............................................................."))
   .catch(err => console.error("Database connection error:", err));
